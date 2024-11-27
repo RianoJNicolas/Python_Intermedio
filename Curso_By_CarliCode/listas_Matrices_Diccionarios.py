de96081bcbd409927699065eee08a66820ffdca4
J. Nicolas Riano T <rianojnicolas@gmail.com>
2024-11-27 17:11:20 -0500

    Adding new challenges

---
+++ Curso_By_CarliCode/listas_Matrices_Diccionarios.py
@@ -0,0 +1,53 @@
+# 1. Doble de los Números. 
+# Dada una lista de números [1, 2, 3, 4, 5], crea una nueva lista 
+# que contenga el doble de cada número usando una List Comprehension.
+
+
+
+
+
+# 2. Filtrar y Transformar en un Solo Paso. 
+# Tienes una lista de palabras ["sol", "mar", "montaña", "rio", "estrella"] 
+# y quieres obtener una nueva lista con las palabras que tengan más de 3 letras 
+# y estén en mayúsculas.
+
+
+
+
+
+# 3. Crear un Diccionario con List Comprehension.
+# Tienes dos listas, una de claves ["nombre", "edad", "ocupación"] y 
+# otra de valores ["Juan", 30, "Ingeniero"]. Crea un diccionario combinando ambas 
+# listas usando una List Comprehension.
+
+
+
+
+# 4. Anidación de List Comprehensions
+# Dada una lista de listas (una matriz): 
+# matriz = [
+#    [1, 2, 3],
+#    [4, 5, 6],
+#    [7, 8, 9]
+# ]
+# Calcula la matriz traspuesta utilizando una List Comprehension anidada.
+
+
+
+# 5. Extraer Información de una Lista de Diccionarios
+# Dada una lista de diccionarios que representan personas:
+# personas = [
+#    {"nombre": "Juan", "edad": 25, "ciudad": "Madrid"},
+#    {"nombre": "Ana", "edad": 32, "ciudad": "Madrid"},
+#    {"nombre": "Pedro", "edad": 35, "ciudad": "Barcelona"},
+#    {"nombre": "Laura", "edad": 40, "ciudad": "Madrid"}
+# ]
+# Extrae una lista de nombres de personas que viven en “Madrid” y tienen más de 30 años.
+
+
+
+
+
+# 6. List Comprehension con un else
+# Dada una lista de números [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], crea una nueva lista 
+# multiplicando por 2 los números pares y dejando los impares como están.
