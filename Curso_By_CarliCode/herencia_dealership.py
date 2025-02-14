83b56a8e3f0da65f6b7314246fd79fcf469648c3
J. Nicolas Riano T <rianojnicolas@gmail.com>
2025-02-13 21:41:04 -0500

    update new file heritage dealership

--- Curso_By_CarliCode/herencia_dealership.py
+++ Curso_By_CarliCode/herencia_dealership.py
@@ -36,3 +36,73 @@ class Car(Vehicle):
             return f"El motor del coche {self.brand} está apagado"
         else:
             return f"El carro {self.brand} no está disponible"
+
+class Bike(Vehicle):
+    def start_engine(self):
+        if not self.available:
+            return f"La bicicleta {self.brand} está en marcha"
+        else:
+            return f"El bicicleta {self.brand} no está disponible"
+    
+    def stop_engine(self):
+        if self.available:
+            return f"La bicicleta {self.brand} se ha detenido" 
+        else:
+            return f"La bicicleta {self.brand} no está disponible"
+
+
+class Truck(Vehicle):
+    def start_engine(self):
+        if not self.available:
+            return f"El motor del camión {self.brand} está en funcionamiento"
+        else:
+            return f"El camión {self.brand} no está disponible"
+    
+    def stop_engine(self):
+        if self.available:
+            return f"El motor del camión {self.brand} está apagado"
+        else:
+            return f"El camión {self.brand} no está disponible"
+
+
+class Customer:
+    def __init__(self, name):
+        self.name = name
+        self.purchased_vehicles = []
+
+    def buy_vehicle(self, vehicle: Vehicle):
+        if vehicle.check_available():
+            vehicle.sell()
+            self.purchased_vehicles.append(vehicle)
+        else:
+            print(f"Lo siento, el vehículo {vehicle.brand} no está disponible")
+
+    def info_vehicles(self, vehicle: Vehicle):
+        if vehicle.check_available():
+            available = "Disponible"
+        else:
+            available = "No disponible"
+        
+        print(f"El vehículo {vehicle.brand} está {available} y cuesta {vehicle.get_price()}")
+
+
+class Dealership:
+    def __init__(self, name):
+        self.name = name
+        self.inventory = []
+        self.customers = []
+
+    def add_vehicle(self, vehicle: Vehicle):
+        self.inventory.append(vehicle)
+        print(f"Se ha añadido el vehículo {vehicle.brand} a la oficina")
+    
+    def register_customer(self, customer: Customer):
+        self.customers.append(customer)
+        print(f"Se ha registrado el cliente {customer.name} a la oficina")
+    
+    def show_inventory(self):
+        print("Los vehículos disponibles en la oficina son:")
+        for vehicle in self.inventory:
+            if vehicle.check_available():
+                print(f"{vehicle.brand} {vehicle.model} por un precio de {vehicle.price}")
+
